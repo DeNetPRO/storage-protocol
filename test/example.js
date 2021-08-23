69e8d19aaf93c69645f6c62ca681da3b415ce772
Denis Shelestov <d@denet.pro>
2021-08-23 18:35:45 +0300

    Deploy Update

--- test/example.js
+++ test/example.js
@@ -4,7 +4,7 @@ const PoS = artifacts.require('ProofOfStorage');
 
 describe('ProofOfStorage', async function () {
     beforeEach(async function () {
-        this.contract = await PoS.new();
+        this.contract = await PoS.new("0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000", "10000000");
     });
 
     it('should be ok', async function () {
